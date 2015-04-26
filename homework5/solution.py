2c735d5cf6fefe9c8daa6ce5e14f67d8c6d16e48
Georgi Dimov <gt_dimov@mail.bg>
2015-04-26 23:59:02 +0300

    Fix BaseFile init and add class File
    Replace ruby initializer in BaseFile and add all methods and attrs
    in File.

--- homework5/solution.py
+++ homework5/solution.py
@@ -1,3 +1,23 @@
 class BaseFile:
-    def initialize(self, is_directory):
+    def __init__(self, is_directory):
         self.is_derectory = is_directory
+
+
+class File(BaseFile):
+    def __init__(self, content):
+        BaseFile.__init__(self, False)
+        self.content = content
+
+    def append(self, text):
+        self.content += text
+
+    def truncate(self, text):
+        self.content = text
+
+    def size(self):
+        return len(self.content) + 1
+f = File("a")
+print(f.content)
+f.truncate("new content")
+print(f.content)
+print(f.size())
