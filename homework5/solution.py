fc62184854fbbb052113d0670768cc989646629d
Georgi Dimov <gt_dimov@mail.bg>
2015-04-28 11:32:59 +0300

    Fix some bugs in File and Directory
    Fix calling size in File.
    Fix calling files and directories in Directory.

--- homework5/solution.py
+++ homework5/solution.py
@@ -1,31 +1,42 @@
 class BaseFile:
     def __init__(self, is_directory):
-        self.is_derectory = is_directory
+        self.is_directory = is_directory
 
 
 class File(BaseFile):
     def __init__(self, content):
         BaseFile.__init__(self, False)
         self.content = content
-        self.size = len(content) + 1
 
     def append(self, text):
         self.content += text
-        self.size += len(text)
 
     def truncate(self, text):
         self.content = text
-        self.size = len(text)
+
+    def __getattr__(self, name):
+        if name == 'size':
+            return len(self.content) + 1
 
 
 class Directory(BaseFile):
     def __init__(self):
         BaseFile.__init__(self, True)
-        self.files = []
-        self.directories = []
-        self.nodes = []
+        self.files = {}
+        self.directories = {}
 
+    def __getattribute__(self, name):
+        if name == 'files' or name == 'directories':
+            return list(self.__dict__[name].keys())
+        else:
+            return BaseFile.__getattribute__(self, name)
 
+    def add_file(self, file_name, file_object):
+        self.__dict__['files'].update({file_name: file_object})
+
+    def add_directory(self, directory_name, directory_object):
+        self.__dict__['directories'].update({directory_name: directory_object})
+'''
 class BaseLink:
     def __init__(self, link_path, symbolic=True):
         self.link_path = link_path
@@ -41,5 +52,30 @@ class HardLink(BaseLink):
     def __init__(self, link_path, symbolic):
         BaseLink.__init__(self, link_path, symbolic)
 
-h = HardLink("/", False)
-print(h.link_path)
+
+class FileSystem:
+    def __init__(self, size):
+        self.size = size
+        self.available_size = size
+        self.home = {"/": Directory()}
+
+    def get_node(self, path):
+        path = path.split('/', 1)[-1]
+        print(path)
+
+        if path in self.home:
+            return self.home[path]
+        else:
+            #print(path.split('/', 1)[-1])
+            print("f")
+f = FileSystem(5)
+f.get_node('/home/georgi/usr/share')
+'''
+d = Directory()
+f = File("first_File_content_here")
+d.add_file("first_file", f)
+d.add_file("second_file", File("second file"))
+print(d.files)
+
+d.add_directory("dir 1", Directory())
+print(d.directories)
